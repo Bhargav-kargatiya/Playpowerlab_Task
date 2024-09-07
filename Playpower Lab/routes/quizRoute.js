5f5e26762c2c6808c89556097ec60330058685fc
Bhargav-kargatiya <bhargav.k1@ahduni.edu.in>
2024-09-07 23:37:15 +0530

    hint controller and generator is added

--- Playpower Lab/routes/quizRoute.js
+++ Playpower Lab/routes/quizRoute.js
@@ -1,14 +1,18 @@
 import express from 'express';
 import { createQuiz, submitQuiz, getQuizHistory, getQuizbyid } from '../controllers/quizController.js';
+import { getHintForQuestion } from '../controllers/bonusController.js';
 
 
 const quizRouter = express.Router();
 
-
+// Necessary API routes
 quizRouter.post('/create', createQuiz);
 quizRouter.post('/submit', submitQuiz);
 quizRouter.get('/history', getQuizHistory);
 quizRouter.post('/retry', submitQuiz);
 quizRouter.get('/oldquiz/:Quizid', getQuizbyid);
+
+// Bonus API routes
+quizRouter.get('/hint/:questionId', getHintForQuestion);
 
 export default quizRouter;
